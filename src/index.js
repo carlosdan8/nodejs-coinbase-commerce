efb7f5e4b896eff533bbec2b1a5c6c22cc7c68db
Dan <icarlosdan8@gmail.com>
2022-03-09 17:33:53 -0600

    First commit

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ const { CB_API_KEY, CB_WH_S, DOMAIN } = require("./config");
 
 const { Client, resources } = require("coinbase-commerce-node");
 
+const morgan = require("morgan");
+
 const PORT = process.env.PORT || 3000;
 
 Client.init(CB_API_KEY);
@@ -11,6 +13,16 @@ Client.init(CB_API_KEY);
 const { Charge } = resources;
 
 const app = express();
+
+app.use(morgan("dev"));
+
+app.use(
+	express.json({
+		verify: (req, res, buf) => {
+			req.rawBody = buf;
+		},
+	})
+);
 
 app.get("/create-charge", async (req, res) => {
 	const chargeData = {
@@ -32,6 +44,36 @@ app.get("/create-charge", async (req, res) => {
 	const charge = await Charge.create(chargeData);
 
 	res.send(charge);
+});
+
+app.post("payment-handler", (req, res) => {
+	const rawBody = req.rawBody;
+	const signature = req.headers["x-cc-webhook-signature"];
+	const webhookSecret = CB_WH_S;
+
+	let event;
+
+	try {
+		event = webhookSecret.verifyEventBody(
+			rawBody,
+			signature,
+			webhookSecret
+		);
+		console.log(event);
+		if (event.type === "charge:pending") {
+			console.info("Charge is pending");
+		}
+		if (event.type === "charge:confirmed") {
+			console.info("Charge is confirmed");
+		}
+		if (event.type === "charge:failed") {
+			console.info("Chargue failed");
+		}
+		return res.status(200).send(event.id);
+	} catch (error) {
+		console.error(error);
+		res.status(400).send("failed");
+	}
 });
 
 app.get("/success-payment", (req, res) => {
